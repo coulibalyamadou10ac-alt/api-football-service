9e7b9c62b45eaef3c31716eaea2daac8b4a5557c
coulibalyamadou10ac-alt <coulibaly.amadou10.ac@gmail.com>
2026-06-12 20:49:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from fastapi import FastAPI
+import os
 import httpx
+from fastapi import FastAPI
 
 app = FastAPI()
-
-# On définit la clé ici directement pour éviter les erreurs de lecture
-API_KEY = "94d2f6a0743159e79b0c2e587b720748"
+# C'est ici que le code "lit" la clé que vous avez mise sur Render
+API_KEY = os.getenv("API_KEY") 
 
 @app.get("/fixtures")
 async def get_fixtures(date: str, timezone: str):
     url = f"https://v3.football.api-sports.io/fixtures?date={date}&timezone={timezone}"
+    # Vérifiez que ces noms de headers sont EXACTS
     headers = {
-        'x-rapidapi-key': API_KEY, 
+        'x-rapidapi-key': str(API_KEY), 
         'x-rapidapi-host': 'v3.football.api-sports.io'
     }
     
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-        return response.json()
-
+        try:
+            response = await client.get(url, headers=headers)
+            return response.json()
+        except Exception as e:
+            return {"error": str(e)}
 
